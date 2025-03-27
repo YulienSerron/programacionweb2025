b9ea14e9aa0687f5a455d3a3f5e00f0d0bf10311
YulienSerron <yulito415@gmail.com>
2025-03-27 14:40:47 -0300

    prueba de comentarios lineal y multilineal, explicacion de variables en php y ejercicio de identificacion de errores en variables

--- hola.php
+++ hola.php
@@ -1,1 +1,21 @@
 <?php echo "hola mundo"; ?>
+<?php// prueba de comentario lineal //
+/* prueba de    
+    comentario  
+    multilineal*/ ?>
+<?php
+    $nombre="manolo"; //texto, puede ser con comillas simples o dobles (String)//
+    $edad= 31; //enteros (Integer)//
+    $altura= 1.71; //Decimales, usando el punto en lugar de la coma(Float)
+    $mayorEdad= true; //Verdadero o Falso (Boolean)//
+?>
+<?php
+//ejercicio//
+$nombreCompleto="Juan Perez";
+$Edad=35;
+$estAtura=1.75; //le pondria la mayuscula a la primera letra//
+$numero="10"; //usa comillas y quiere definir un int//
+Apellido='Fernandez'; //le falta el signo de $ (signo de la variable)//
+pais_de_origen="Uruguay"; //le falta el signo de $ (signo de la variable)//
+$salario=$2000; //usa el signo de $ y define otra variable//
+?>
