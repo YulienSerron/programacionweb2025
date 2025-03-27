33f25370ea61d4701826413c796bd0a6ebac7735
YulienSerron <yulito415@gmail.com>
2025-03-27 15:30:12 -0300

    Creamos constante, y respondemos las preguntas del ejercicio

---
+++ constante.php
@@ -0,0 +1,14 @@
+<?php 
+const pi = 3.1416;
+const pi = 3.14;
+//error constante pi ya esta definida//
+/*
+respuesta 1 que la constante ya esta definida
+respuesta 2 nos indica que una vez que asignamos el valor a la constante no se le puede cambiar a menos que lo cambiemos donde la definimos por primera vez
+respuesta 3 cuando quieras multiplicar algo por un numero que no cambia por ejemplo la gravedad (9.8)
+*/
+?>
+
+
+
+
