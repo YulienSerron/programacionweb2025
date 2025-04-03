db9dc700010c358af3769d86368499ada17696c7
YulienSerron <yulito415@gmail.com>
2025-04-03 14:14:27 -0300

    Operadores y como funcionan

---
+++ Operadores.php
@@ -0,0 +1,16 @@
+<?php
+$edad=1;
+$edad+=1;
+// cuando es += a edad le suma y asigna lo que esta a la derecha
+$edad-=1;
+// cuando es -= a edad le resta y asigna lo que esta a la derecha
+$edad*=1;
+// cuando es *= a edad lo multiplica y asigna por lo que esta a la derecha
+$edad/=1;
+// cuando es /= a edad lo divide y asigna por lo que esta a la derecha
+$saludo = 'hola';
+$saludo.= '_yulien';
+// cuando es .= concatena los textos
+
+
+?>
