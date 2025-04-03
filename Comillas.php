77583c2aa93f9d01791b47e7badba9d349b99ac9
YulienSerron <yulito415@gmail.com>
2025-04-03 13:56:31 -0300

    Funcion de comillas dobles y simples

---
+++ Comillas.php
@@ -0,0 +1,19 @@
+<?php $emisora = 'Radio Disney';
+echo "Me gusta escuchar $emisora ";
+
+echo 'Me gusta escuchar $emisora';
+
+/*Comillas Dobles: Permiten la interpolación de variables dentro de la cadena. 
+Esto significa que las variables dentro de comillas dobles serán evaluadas y reemplazadas por sus valores.
+
+Comillas Simples: No permiten la interpolación de variables. 
+Las variables dentro de comillas simples se tratarán como texto literal.
+*/
+
+
+
+
+
+
+
+?>
